9ca2ad2615e41a8b66fd7fbbbada73ec98bbe52d
Nandmp04 <nandanapersonal04@gmail.com>
2025-06-02 00:32:45 +0530

    Update predict.py

--- api/predict.py
+++ api/predict.py
@@ -3,37 +3,22 @@ import json
 
 class handler(BaseHTTPRequestHandler):
     def do_POST(self):
-        try:
-            # Read request body
-            content_length = int(self.headers.get('Content-Length', 0))
-            body = self.rfile.read(content_length)
-            data = json.loads(body)
+        content_length = int(self.headers.get('Content-Length', 0))
+        body = self.rfile.read(content_length)
+        data = json.loads(body)
 
-            # Read input values
-            savings = data.get("savings", 0)
-            goal = data.get("goal", "")
-            scheme = data.get("scheme", "")
+        savings = data.get("savings", 0)
+        if savings < 200000:
+            color = "red"
+            tip = "Save more"
+        elif savings < 500000:
+            color = "orange"
+            tip = "Almost there"
+        else:
+            color = "green"
+            tip = "You're ready!"
 
-            # Simple logic
-            if savings < 200000:
-                color = "red"
-                tip = "You need to save more."
-            elif savings < 500000:
-                color = "orange"
-                tip = "You're getting closer!"
-            else:
-                color = "green"
-                tip = "You're financially ready."
-
-            # Return result
-            result = {"color": color, "tip": tip}
-            self.send_response(200)
-            self.send_header("Content-Type", "application/json")
-            self.end_headers()
-            self.wfile.write(json.dumps(result).encode())
-
-        except Exception as e:
-            self.send_response(500)
-            self.send_header("Content-Type", "application/json")
-            self.end_headers()
-            self.wfile.write(json.dumps({"error": str(e)}).encode())
+        self.send_response(200)
+        self.send_header("Content-Type", "application/json")
+        self.end_headers()
+        self.wfile.write(json.dumps({"color": color, "tip": tip}).encode())
