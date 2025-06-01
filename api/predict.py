4e5e6cb434e78059f48fdda7cb23bd523d3ddbee
Nandmp04 <nandanapersonal04@gmail.com>
2025-06-01 19:50:02 +0530

    Update predict.py

--- api/predict.py
+++ api/predict.py
@@ -1,11 +1,9 @@
-# api/predict.py
-
 from http.server import BaseHTTPRequestHandler
 import json
 
 class handler(BaseHTTPRequestHandler):
     def do_POST(self):
-        content_length = int(self.headers['Content-Length'])
+        content_length = int(self.headers.get('Content-Length', 0))
         post_data = self.rfile.read(content_length)
         data = json.loads(post_data)
 
@@ -23,12 +21,7 @@ class handler(BaseHTTPRequestHandler):
             color = "red"
             tip = "Savings too low for this goal."
 
-        response = {
-            "color": color,
-            "tip": tip
-        }
-
         self.send_response(200)
         self.send_header('Content-type', 'application/json')
         self.end_headers()
-        self.wfile.write(json.dumps(response).encode())
+        self.wfile.write(json.dumps({"color": color, "tip": tip}).encode())
